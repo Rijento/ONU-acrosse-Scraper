4c0ee9505069b787c581e0174b11f12902bce77c
rijento <rijento@gmial.com>
2021-11-17 13:00:37 -0500

    table updates with combo box

--- src/TestController1.java
+++ src/TestController1.java
@@ -1,31 +1,43 @@
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.IOException;
 
-public class TestController1 implements ItemListener, ActionListener, ListSelectionListener {
+public class TestController1 implements ItemListener, ListSelectionListener {
     TestView1 view;
-    public TestController1(TestView1 view) {
+    Team currentTeam;
+    public TestController1(TestView1 view) throws IOException, InterruptedException {
         this.view = view;
+        this.currentTeam = WebScraper.getYearTeam("2015-16");
     }
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == this.view.test) {
-            this.view.test.setText("DONE");
-        }
+    public void setFirstDisplay() {
+        this.view.playerTableItemModel.setPlayers(currentTeam.getPlayers());
     }
 
     @Override
     public void itemStateChanged(ItemEvent itemEvent) {
-
+        if (itemEvent.getSource() == this.view.yearDropdown) {
+            String year = itemEvent.getItem().toString();
+            if (!currentTeam.year.equals(year)) {
+                try {
+                    currentTeam = WebScraper.getYearTeam(year);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                this.view.playerTableItemModel.setPlayers(currentTeam.getPlayers());
+                this.view.playerTable.repaint();
+                this.view.playerTable.revalidate();
+            }
+        }
     }
 
     @Override
     public void valueChanged(ListSelectionEvent listSelectionEvent) {
-        int i = this.view.playerTable.getSelectedColumn();
+        int i = this.view.playerTable.getSelectedRow();
         Player selected = this.view.playerTableItemModel.getPlayer(i);
         int test = 1;
     }
--- src/TestView1.java
+++ src/TestView1.java
@@ -5,35 +5,40 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TestView1 extends JFrame {
     JButton test;
     JTable playerTable;
     TestController1 controller;
     PlayerTableItemModel playerTableItemModel;
-    Team currentTeam;
+    JComboBox yearDropdown;
     public TestView1() throws IOException, InterruptedException {
         super();
-
-        this.currentTeam = WebScraper.getYearTeam("2015-16");
         this.controller = new TestController1(this);
-        this.test = new JButton("test");
+//        this.test = new JButton("test");
+        String[] options = {"2015-16", "2016-17", "2017-18", "2018-19", "2019-20", "2020-21"};
+        this.yearDropdown = new JComboBox(options);
+        this.yearDropdown.addItemListener(controller);
+
         this.playerTable = new JTable();
-        this.playerTableItemModel = new PlayerTableItemModel(currentTeam.getPlayers());
+        this.playerTableItemModel = new PlayerTableItemModel(new ArrayList());
 
         this.playerTable.setModel(playerTableItemModel);
         this.playerTable.getSelectionModel().addListSelectionListener(controller);
 
         JScrollPane scrollPane = new JScrollPane(playerTable);
 
-        this.test.addActionListener(this.controller);
         this.setLayout(new FlowLayout());
 
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        this.add(test);
+//        this.add(test);
+        this.add(yearDropdown);
         this.add(scrollPane);
         this.setSize(400, 300);
+
+        this.controller.setFirstDisplay();
     }
 
 
